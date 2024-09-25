ca6bce09681aaeac790e0dabf7b3a21854ecaf56
kyzyc <yuchenzhu66@gmail.com>
2024-09-25 19:10:27 +0800

    feature: pass the string.c test.
    add some string related function like strcpy, strcat, memset, etc.

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,15 +1,18 @@
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdint.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
-  panic("Not implemented");
-}
+size_t strlen(const char *s) { panic("Not implemented"); }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *p = dst;
+  for (; *src != '\0'; p++, src++) {
+    *p = *src;
+  }
+  *p = '\0';
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,11 +20,29 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *p = dst;
+  while ((*p) != '\0') p++;
+  for (; *src != '\0'; p++, src++) {
+    *p = *src;
+  }
+  *p = '\0';
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  const char *p = s1, *q = s2;
+  while ((*p) != '\0' && (*p) == (*q)) {
+    p++;
+    q++;
+  }
+
+  if ((*p) == (*q)) {
+    return 0;
+  } else if ((*p) > (*q)) {
+    return 1;
+  }
+
+  return -1;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,19 +50,30 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  char *p = s;
+  for (size_t i = 0; i < n; i++, p++) {
+    (*p) = c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
   panic("Not implemented");
 }
 
-void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
-}
+void *memcpy(void *out, const void *in, size_t n) { panic("Not implemented"); }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const char *p = s1, *q = s2;
+  size_t i;
+  for (i = 0; (*p) == (*q) && i < n; i++, p++, q++);
+
+  if (i == n || (*p) == (*q)) {
+    return 0;
+  } else if ((*p) > (*q)) {
+    return 1;
+  }
+  return -1;
 }
 
 #endif
