1218055810328f50ad2ccfc289a733af6a796d05
kyzyc <yuchenzhu66@gmail.com>
2024-09-25 20:09:43 +0800

    format: format stdio.h

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,13 +1,11 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
-}
+int printf(const char *fmt, ...) { panic("Not implemented"); }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
@@ -17,14 +15,14 @@ int sprintf(char *out, const char *fmt, ...) {
   va_list ap;
   union {
     int iarg;
-    const char* carg;
+    const char *carg;
   } arg;
-  char* q = out;
+  char *q = out;
   int n;
 
   va_start(ap, fmt);
-  
-  for (const char* p = fmt; (*p) != '\0'; p++) {
+
+  for (const char *p = fmt; (*p) != '\0'; p++) {
     if ((*p) == '%') {
       p++;
       switch (*p) {
