8f6e45914d80b89f4de7c2e93fffa62cce56304b
kyzyc <yuchenzhu66@gmail.com>
2024-12-10 19:23:53 +0800

    feature: triger yield.

--- nemu/src/isa/riscv32/local-include/reg.h
+++ nemu/src/isa/riscv32/local-include/reg.h
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #ifndef __RISCV_REG_H__
 #define __RISCV_REG_H__
@@ -21,6 +21,21 @@
 static inline int check_reg_idx(int idx) {
   IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx < MUXDEF(CONFIG_RVE, 16, 32)));
   return idx;
+}
+
+static inline int map_csrs_idx(int idx) {
+  switch (idx) {
+    case 0x305:
+      return 0;
+    case 0x341:
+      return 1;
+    case 0x300:
+      return 2;
+    case 0x342:
+      return 3;
+    default:
+      Assert(0, "unknown csrs reg");
+  }
 }
 
 #define gpr(idx) (cpu.gpr[check_reg_idx(idx)])
