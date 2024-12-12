c2bab81a8a9b246ef35e22b3ab6f247f1affc507
kyzyc <yuchenzhu66@gmail.com>
2024-12-12 18:38:22 +0800

    feature: add machine guard.

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -1,6 +1,6 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
@@ -8,23 +8,21 @@ static unsigned long int next = 1;
 int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
+  return (unsigned int)(next / 65536) % 32768;
 }
 
-void srand(unsigned int seed) {
-  next = seed;
-}
+void srand(unsigned int seed) { next = seed; }
 
-int abs(int x) {
-  return (x < 0 ? -x : x);
-}
+int abs(int x) { return (x < 0 ? -x : x); }
 
-int atoi(const char* nptr) {
+int atoi(const char *nptr) {
   int x = 0;
-  while (*nptr == ' ') { nptr ++; }
+  while (*nptr == ' ') {
+    nptr++;
+  }
   while (*nptr >= '0' && *nptr <= '9') {
     x = x * 10 + *nptr - '0';
-    nptr ++;
+    nptr++;
   }
   return x;
 }
@@ -33,27 +31,25 @@ void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  static char* hbrk = NULL;
-//   // panic("Not implemented");
+  // #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+  static char *hbrk = NULL;
   size = (size_t)ROUNDUP(size, 8);
   if (hbrk == NULL) {
     hbrk = heap.start;
   }
 
-  char* old = hbrk;
+  char *old = hbrk;
   hbrk += size;
-  assert((uintptr_t)heap.start <= (uintptr_t)hbrk && (uintptr_t)hbrk < (uintptr_t)heap.end);
-  for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)hbrk; p ++) {
+  assert((uintptr_t)heap.start <= (uintptr_t)hbrk &&
+         (uintptr_t)hbrk < (uintptr_t)heap.end);
+  for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)hbrk; p++) {
     *p = 0;
   }
 
   return old;
-#endif
-  return NULL;
+  // #endif
 }
 
-void free(void *ptr) {
-}
+void free(void *ptr) {}
 
 #endif
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,19 +11,27 @@
 #define Elf_Phdr Elf32_Phdr
 #endif
 
+#if defined(__ISA_AM_NATIVE__)
+#define EXPECT_TYPE EM_X86_64
+#elif defined(__riscv)
+#define EXPECT_TYPE EM_RISCV  // see /usr/include/elf.h to get the right type
+#else
+#error Unsupported ISA
+#endif
+
 extern uint8_t ramdisk_start;
 
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
-static Elf32_Ehdr *read_elf_header() {
-  Elf32_Ehdr *elf_header = (Elf32_Ehdr *)malloc(sizeof(Elf32_Ehdr));
-  assert(ramdisk_read(elf_header, 0, sizeof(Elf32_Ehdr)));
+static Elf_Ehdr *read_elf_header() {
+  Elf_Ehdr *elf_header = (Elf_Ehdr *)malloc(sizeof(Elf_Ehdr));
+  assert(ramdisk_read(elf_header, 0, sizeof(Elf_Ehdr)));
   return elf_header;
 }
 
-static Elf32_Phdr *read_program_header(Elf32_Ehdr *eh) {
-  Elf32_Phdr *ph_table = (Elf32_Phdr *)malloc(eh->e_phnum * eh->e_phentsize);
+static Elf_Phdr *read_program_header(Elf_Ehdr *eh) {
+  Elf_Phdr *ph_table = (Elf_Phdr *)malloc(eh->e_phnum * eh->e_phentsize);
   uint32_t i;
 
   for (i = 0; i < eh->e_phnum; i++) {
@@ -35,7 +43,7 @@ static Elf32_Phdr *read_program_header(Elf32_Ehdr *eh) {
   return ph_table;
 }
 
-static bool iself(Elf32_Ehdr *eh) {
+static bool iself(Elf_Ehdr *eh) {
   /* ELF magic bytes are 0x7f,'E','L','F'
    * Using  octal escape sequence to represent 0x7f
    */
@@ -52,18 +60,22 @@ static bool iself(Elf32_Ehdr *eh) {
 }
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  Elf32_Ehdr *elf_header = read_elf_header();
+  Elf_Ehdr *elf_header = read_elf_header();
   if (!iself(elf_header)) {
     free(elf_header);
-    assert("not an elf file!");
+    assert(0);
+  }
+
+  if (elf_header->e_machine != EXPECT_TYPE) {
+    assert(0);
   }
 
   uintptr_t start_addr = elf_header->e_entry;
 
-  Elf32_Phdr *ph_tabl = read_program_header(elf_header);
+  Elf_Phdr *ph_tabl = read_program_header(elf_header);
 
   for (int i = 0; i < elf_header->e_phnum; i++) {
-    Elf32_Phdr ph = ph_tabl[i];
+    Elf_Phdr ph = ph_tabl[i];
     if (ph.p_type == PT_LOAD) {
       memcpy((void *)ph.p_vaddr, (void *)(&ramdisk_start + ph.p_offset),
              ph.p_filesz);
