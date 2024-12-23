12fbe1a3e8ad0e93e2295928777c9843ddaca548
kyzyc <yuchenzhu66@gmail.com>
2024-12-23 18:09:09 +0800

    menu

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -69,7 +69,7 @@ static void clear_display(void) {
 
 int main(int argc, char *argv[], char *envp[]) {
   SDL_Init(0);
-  screen = SDL_SetVideoMode(0, 0, 32, SDL_HWSURFACE);
+  screen = SDL_SetVideoMode(400, 300, 32, SDL_HWSURFACE);
 
   font = new BDF_Font(font_fname);
   logo_sf = SDL_LoadBMP("/share/pictures/projectn.bmp");
@@ -142,7 +142,7 @@ static void draw_text_row(char *s, int r) {
 static void display_menu(int n) {
   clear_display();
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
-  SDL_BlitSurface(logo_sf, NULL, screen, &rect);
+  SDL_BlitSurface(logo_sf, NULL, screen, &rect); // this
   printf("Available applications:\n");
   char buf[80];
   int i;
